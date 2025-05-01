51c648f8dff42ab6433b1bc428fa298b4b8ba333
Alieva Nargiza <nargiza.alieva018@gmail.com>
2025-05-02 01:18:32 +0600

    create token entity and repository

---
+++ src/main/java/org/example/schoolapp/entity/Token.java
@@ -0,0 +1,35 @@
+package org.example.schoolapp.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.schoolapp.enums.TokenType;
+
+@Entity
+@Table(name = "tokens")
+@Builder(toBuilder = true)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "token")
+    private String token;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "token_type")
+    private TokenType tokenType;
+
+    @Column(name = "is_expired")
+    private boolean expired;
+
+    @Column(name = "is_revoked")
+    private boolean revoked;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
---
+++ src/main/java/org/example/schoolapp/enums/TokenType.java
@@ -0,0 +1,5 @@
+package org.example.schoolapp.enums;
+
+public enum TokenType {
+    Bearer;
+}
